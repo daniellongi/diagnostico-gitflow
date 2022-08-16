5536a1ac7675cc11f68f050eca6c20d43bbce38d
Daniel Vildosola <djvildosola@uc.cl>
2022-08-16 01:00:45 -0400

    funcion top 10 usuarios

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ def top_retweeted():
     #funcion que retorna los top 10 tweets mas retweeted
     pass
 
-
-
-
+def top_usuarios():
+    #funcion que retorna los top 10 usuarios en funcion de la cantidad de tweets que emitieron
+    pass
 
 def main():
+    top_retweeted()
+    top_usuarios()
     #funcion que llama a las otras funciones
     pass
