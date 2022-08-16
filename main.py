2b9f9db55cc9b2882f6d56be10a017cc3dbcac18
Daniel Vildosola <djvildosola@uc.cl>
2022-08-16 01:04:11 -0400

    funcion hashtag

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import pandas as pd
-from pandas.io.json import json_normalize
-import warnings
-warnings.filterwarnings("ignore")
+
 def top_retweeted():
     #funcion que retorna los top 10 tweets mas retweeted
     pass
@@ -14,9 +11,14 @@ def top_dias():
     #funcion que retorna top 10 dias donde hay mas tweets
     pass
 
+def hashtag():
+    #funcion que retorna los 10 hashtag mas usados
+    pass
+
 def main():
     top_retweeted()
     top_usuarios()
     top_dias()
+    hashtag()
     #funcion que llama a las otras funciones
     pass
