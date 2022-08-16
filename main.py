5d767e3c69727ed29ffb389c8d1fb3980c44d42a
Daniel Vildosola <djvildosola@uc.cl>
2022-08-16 00:58:29 -0400

    funcion top 10 retweets

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from pandas.io.json import json_normalize
+import warnings
+warnings.filterwarnings("ignore")
+def top_retweeted():
+    #funcion que retorna los top 10 tweets mas retweeted
+    pass
+
+
+
+
+
+def main():
+    #funcion que llama a las otras funciones
+    pass
